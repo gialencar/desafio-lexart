31ffae6433286734581d72f0f10bad9934ae73d4
Gilson Alencar <gilson.alencar94@gmail.com>
2024-07-20 17:02:51 -0300

    fix(config): server host and port configuration typo

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -5,5 +5,7 @@ dotenv.config();
 export const DEVELOPMENT = process.env.NODE_ENV === 'development';
 export const TEST = process.env.NODE_ENV === 'test';
 
-export const SERVER_HOST = process.env.SERVER_HOSTNAME || 'localhost';
-export const SERVER_PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
+export const SERVER_HOST = process.env.SERVER_HOST || 'localhost';
+export const SERVER_PORT = process.env.SERVER_PORT
+  ? Number(process.env.SERVER_PORT)
+  : 3000;
