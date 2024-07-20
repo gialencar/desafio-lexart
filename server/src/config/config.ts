aad8b74f1cd357c3b1b3497d0657a8d540d377f9
Gilson Alencar <gilson.alencar94@gmail.com>
2024-07-20 15:29:14 -0300

    chore(server): Add configuration files

---
+++ server/src/config/config.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const DEVELOPMENT = process.env.NODE_ENV === 'development';
+export const TEST = process.env.NODE_ENV === 'test';
+
+export const SERVER_HOST = process.env.SERVER_HOSTNAME || 'localhost';
+export const SERVER_PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
