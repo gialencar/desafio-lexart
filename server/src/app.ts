df6868b233dfd1fa385df69372060b75d3b83360
Gilson Alencar <gilson.alencar94@gmail.com>
2024-07-21 17:55:56 -0300

    feat: Add index router to handle routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,9 @@
 import cors from 'cors';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import routeNotFound from './middlewares/routeNotFound';
+import router from './routes';
 
 const app = express();
 
@@ -10,11 +11,16 @@ app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(router);
 
 app.get('/healthcheck', (_req, res) => {
   res.json({ message: 'Server is running' });
 });
 
 app.use(routeNotFound);
+
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  res.status(500).json({ error: {message: err.message} });
+});
 
 export default app;
