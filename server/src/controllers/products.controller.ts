3b6d0db03e494150de0f86f7a67867548a166756
Gilson Alencar <gilson.alencar94@gmail.com>
2024-07-22 01:50:44 -0300

    feat: Add ProductsController

---
+++ server/src/controllers/products.controller.ts
@@ -0,0 +1,54 @@
+import { NextFunction, Request, Response } from 'express';
+import ProductsService from '../services/products.service';
+
+export class ProductsController {
+  private service = new ProductsService();
+
+  async getAll(_req: Request, res: Response, next: NextFunction) {
+    try {
+      const users = await this.service.getAll();
+      res.status(200).json(users);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = parseInt(req.params.id);
+      const user = await this.service.getById(id);
+      res.status(200).json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const user = await this.service.create(req.body);
+      res.status(201).json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async update(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = parseInt(req.params.id);
+      const user = await this.service.update(id, req.body);
+      res.status(200).json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async delete(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = parseInt(req.params.id);
+      await this.service.delete(id);
+      res.status(204).end();
+    } catch (error) {
+      next(error);
+    }
+  }
+}
